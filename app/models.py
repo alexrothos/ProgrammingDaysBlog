1843c24f69b394ad9d6f364d9606b8cf1e421127
Alex Rothos <alexrothos@gmail.com>
2021-12-10 13:05:12 +0200

    Rewrite of User class after many errors...

--- app/models.py
+++ app/models.py
@@ -1,17 +1,8 @@
-from app import db
 from datetime import datetime
+from app import db
 
 
-class Student(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.String(64), index=True, unique=True)
-    email = db.Column(db.String(120), index=True, unique=True)
-    password_hash = db.Column(db.String(128))
-
-    def __repr__(self):
-        return '<Student {}>'.format(self.username)
-
-class Teacher(db.Model):
+class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(64), index=True, unique=True)
     email = db.Column(db.String(120), index=True, unique=True)
@@ -19,12 +10,13 @@ class Teacher(db.Model):
     posts = db.relationship('Post', backref='author', lazy='dynamic')
 
     def __repr__(self):
-        return '<Teacher {}>'.format(self.username)
+        return '<User {}>'.format(self.username)
+
 
 class Post(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    body = db.Column(db.String(300))
-    timestamp = db.Column(db.Datetime, index=True, default=datetime.utcnow)
+    body = db.Column(db.String(140))
+    timestamp = db.Column(db.DateTime, index=True, default=datetime.utcnow)
     user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
 
     def __repr__(self):
--- config.py
+++ config.py
@@ -15,5 +15,6 @@ class Config(object):
     ADMINS = ['alexrothos@gmail.com']
 
     # DB configuration
-    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or 'sqlite:///'+os.path.join(basedir, 'app.db')
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
+        'sqlite:///' + os.path.join(basedir, 'app.db')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
