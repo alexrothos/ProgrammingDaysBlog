dd60b36a946f72178fb804022afb157a00f09505
Alex Rothos <alexrothos@gmail.com>
2021-12-10 11:22:48 +0200

    Req & login form

--- app/routes.py
+++ app/routes.py
@@ -1,5 +1,5 @@
 from app import app
-from flask import render_template
+from flask import render_template, flash, redirect, url_for
 from app.forms import LoginForm
 
 @app.route('/', methods=['GET','POST'])
@@ -7,7 +7,10 @@ from app.forms import LoginForm
 def index():
     return render_template("index.html", title=None)
 
-@app.route('/login')
+@app.route('/login', methods=['GET', 'POST'])
 def login():
     form = LoginForm()
+    if form.validate_on_submit():
+        flash('Login  - {} @ remember me status - {}'.format(form.username.data, form.remember_me.data))
+        return redirect(url_for('index'))
     return render_template('login.html', title='Sign In', form=form)
