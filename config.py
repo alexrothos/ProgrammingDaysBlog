e4be328c909eb08040fc9f95d112940fd3ff892b
Alex Rothos <alexrothos@gmail.com>
2021-12-10 12:35:44 +0200

    DB creation & Teacher class creation

--- config.py
+++ config.py
@@ -15,3 +15,5 @@ class Config(object):
     ADMINS = ['alexrothos@gmail.com']
 
     # DB configuration
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or 'sqlite:///'+os.path.join(basedir, 'app.db')
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
