3472dce3c875b7894ef53d2a59d485602e439005
Alex Rothos <alexrothos@gmail.com>
2021-12-29 20:16:28 +0200

    Making Users profile info

--- app/routes.py
+++ app/routes.py
@@ -1,10 +1,11 @@
 from flask_login.utils import logout_user
 from app import app, db
 from flask import render_template, flash, redirect, url_for, request
-from app.forms import LoginForm, RegistrationForm
+from app.forms import LoginForm, RegistrationForm, EditProfileForm
 from flask_login import current_user, login_user, logout_user, login_required
 from app.models import User
 from werkzeug.urls import url_parse
+from datetime import datetime
 
 
 @app.route('/', methods=['GET','POST'])
@@ -66,3 +67,25 @@ def user(username):
     user = User.query.filter_by(username=username).first_or_404()
     posts = []
     return render_template('user.html', user=user, posts=posts)
+
+# here is the time registration of users
+@app.before_request     # the before_request is to be executed right before any request
+def before_request():
+    if current_user.is_authenticated:
+        current_user.last_seen = datetime.utcnow() # the current_user is already loaded, so no need to add to db
+        db.session.commit() # the db.session.add is unecessary
+
+@app.route('/edit_profile', methods=['GET', 'POST'])
+@login_required
+def edit_profile():
+    form = EditProfileForm()
+    if form.validate_on_submit():
+        current_user.username = form.username.data
+        current_user.about_me = form.about_me.data
+        db.session.commit()
+        flash('Your changes have been saved.')
+        return redirect(url_for('edit_profile'))
+    elif request.method == 'GET':
+        form.username.data = current_user.username
+        form.about_me.data = current_user.about_me
+    return render_template('edit_profile.html', title='Edit Profile', form=form)
