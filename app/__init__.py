6c52ee3afbd8c2d8ff0dba7b55130efaf359cf55
Alex Rothos <alexrothos@gmail.com>
2021-12-29 20:40:31 +0200

    Error handling & user profile edittor username check

--- app/__init__.py
+++ app/__init__.py
@@ -3,6 +3,9 @@ from config import Config
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from flask_login import LoginManager
+import logging
+from logging.handlers import SMTPHandler, RotatingFileHandler
+import os
 
 app = Flask(__name__)                # creation of application
 app.config.from_object(Config)       # connection with the configuration file
@@ -11,4 +14,26 @@ migrate = Migrate(app, db)
 login = LoginManager(app)            # starting the login validation process
 login.login_view = 'login'           # the LoginManager, named login now, manage the view for user not yet logged in, so they redirect to login page and back after the succesful log, the name 'login' is entering in a url_for
 
-from app import routes               # import the main navigation control for the app
+if not app.debug:
+    if app.config['MAIL_SERVER']:
+        auth = None
+        if app.config['MAIL_USERNAME'] or app.config['MAIL_PASSWORD']:
+            auth = (app.config['MAIL_USERNAME'], app.config['MAIL_PASSWORD'])
+        secure = None
+        if app.config('MAIL_USE_TLS'):
+            secure = ()
+        mail_handler = SMTPHandler(mailhost=(app.config['MAIL_SERVER'], app.config['MAIL_PORT']), fromaddr='no-reply@' + app.config['MAIL_SERVER'], toaddrs=app.config['ADMINS'], subject='ProgDay Failure', credentials=auth, secure=secure)
+        mail_handler.setLevel(logging.ERROR)
+        app.logger.addHandler(mail_handler) 
+    
+    if not os.path.exists('logs'):
+        os.mkdir('logs')
+    file_handler = RotatingFileHandler('logs/progday.log', maxBytes=10240, backupCount=10)
+    file_handler.setFormatter(logging.Formatter('%(asctime)s %(levelname)s: %(message)s [in %(pathname)s:%(lineno)d]'))
+    file_handler.setLevel(logging.INFO)
+    app.logger.addHandler(file_handler)
+
+    app.logger.setLevel(logging.INFO)
+    app.logger.info('ProgDay startup')
+
+from app import routes, models, errors               # import the main navigation control for the app
--- config.py
+++ config.py
@@ -5,11 +5,12 @@ class Config(object):
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'knowledge-is-the-key'
 
     # Mail configuration
-    MAIL_SERVER = os.environ.get('???')
+    MAIL_SERVER = os.environ.get('MAIL_SERVER')
     MAIL_PORT = int(os.environ.get('MAIL_PORT') or 25)
     MAIL_USE_TLS = os.environ.get('MAIL_USE_TLS') is not None
-    MAIL_USERNAME = os.environ.get('???')
-    MAIL_PASSWORD = os.environ.get('???')
+    MAIL_USERNAME = os.environ.get('MAIL_USERNAME')
+    MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD')
+    ADMINS = ['progday@gmail.com'] # must make an email for the blog...
     
     # Misc configurations
     ADMINS = ['alexrothos@gmail.com']
