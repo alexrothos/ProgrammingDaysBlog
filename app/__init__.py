77b79e656b0d0b8f78eda8c58b35ad3ddb0817d1
Alex Rothos <alexrothos@gmail.com>
2021-12-29 19:32:43 +0200

    Add the user registration and some comments

--- app/__init__.py
+++ app/__init__.py
@@ -4,10 +4,11 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from flask_login import LoginManager
 
-app = Flask(__name__)
-app.config.from_object(Config)
-db = SQLAlchemy(app)
+app = Flask(__name__)                # creation of application
+app.config.from_object(Config)       # connection with the configuration file
+db = SQLAlchemy(app)                 # define the database, app.db in root folder
 migrate = Migrate(app, db)
-login = LoginManager(app)
+login = LoginManager(app)            # starting the login validation process
+login.login_view = 'login'           # the LoginManager, named login now, manage the view for user not yet logged in, so they redirect to login page and back after the succesful log, the name 'login' is entering in a url_for
 
-from app import routes
+from app import routes               # import the main navigation control for the app
--- app/routes.py
+++ app/routes.py
@@ -1,24 +1,60 @@
-from app import app
-from flask import render_template, flash, redirect, url_for
-from app.forms import LoginForm
-from flask_login import current_user, login_user
+from flask_login.utils import logout_user
+from app import app, db
+from flask import render_template, flash, redirect, url_for, request
+from app.forms import LoginForm, RegistrationForm
+from flask_login import current_user, login_user, logout_user, login_required
 from app.models import User
+from werkzeug.urls import url_parse
+
 
 @app.route('/', methods=['GET','POST'])
 @app.route('/index', methods=['GET','POST'])
+@login_required                                     # to protect the function to be used without validation
 def index():
-    return render_template("index.html", title=None)
+    return render_template("index.html", title='Home page of Programming Days Blog', posts=posts)
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
+    # if the user is already logged, redirecting to index page
     if current_user.is_authenticated:
         return redirect(url_for('index'))
+    
     form = LoginForm()
+
     if form.validate_on_submit():
         user = User.query.filter_by(username=form.username.data).first()
         if user is None or not user.check_password(form.password.data):
-            flash('Invalid username or passwor')
+            flash('Invalid username or password')
             return redirect(url_for('login'))
         login_user(user, remember=form.remember_me.data)
-        return redirect(url_for('index'))
+        
+        # here starts the handling for the "next" in url /login?next=/index query
+        next_page = request.args.get('next')
+        if not next_page or url_parse(next_page).netloc != '': # securing with url_parse the redirect
+            next_page = url_for('index')
+        
+        return redirect(next_page)
     return render_template('login.html', title='Sign In', form=form)
+
+@app.route('/logout')
+def logout():
+    logout_user()
+    return redirect(url_for('index'))
+
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+    if current_user.is_authenticated:
+        return redirect(url_for('index'))
+    form = RegistrationForm()
+    if form.validate_on_submit():
+        # setting the user's data in place
+        user = User(username=form.username.data, email=form.email.data)
+        user.set_password(form.password.data)
+        
+        # adding the new user to the database
+        db.session.add(user)
+        db.session.commit()
+
+        flash('Registration succeful!')
+        return redirect(url_for('login'))
+    return render_template('register.html', title='Student Registration', form=form)
